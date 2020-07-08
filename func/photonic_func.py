8eee6dd21a96a3f42b0688986e011e2b9db37b32
David Cohen <davidxcohen@SigalitsMacBook.lan>
2020-07-09 00:19:04 +0300

    Add ambient light, start adding noise

--- func/photonic_func.py
+++ func/photonic_func.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import sys, os
-from scipy import signal
+from scipy import signal, interpolate
 
 nm = 1e-9
 c = 2.99792458e8  # [m/s] Speed of light
@@ -12,6 +12,8 @@ h   = 6.62606957e-34 # [J*sec] Plank constant
 nm = 1e-9 # [m]
 cm = 1e-2 # [m]
 e_minus =1.60217662e-19  # [Culomb]
+T_kelvin = 300
+maximum_amb_light_lux = 120000 # [lux]
 
 
 def PlankLawBlackBodyRad(T, wavelength):
@@ -68,9 +70,52 @@ class Photonic:
 		self.wall_flux = self.wallFlux()
 		self.silicon_flux = self.siliconFlux(self.wall_flux)
 
-	def wallFlux(self, light=None, scene=None, dist_vec=None, light_type='ir_pulse'):
+	def solarSpectrum_W_m2_nm(self):
+		# NASA technical Memorandum1980
+		# https://ntrs.nasa.gov/archive/nasa/casi.ntrs.nasa.gov/19810016493.pdf
+		solar_spectrum_radiation_distribution = \
+			[0.3138, 11.31, 0.3205, 108.0, 0.3307, 204.7, 0.3409, 276.0, 0.3544, 337.1, 0.3679, 393.2, 
+			0.3713, 428.8, 0.3883, 469.7, 0.395, 551.1, 0.3984, 647.8, 0.4086, 790.2, 0.4086, 836.0, 
+			0.4221, 871.7, 0.4323, 866.7, 0.4391, 953.3, 0.4492, 1121, 0.4526, 1187, 0.4594, 1213, 0.4695, 
+			1228, 0.4797, 1264, 0.4865, 1238, 0.5, 1249, 0.5102, 1229, 0.5169, 1208, 0.5271, 1224, 0.5474, 
+			1183, 0.5542, 1178, 0.5609, 1168, 0.5711, 1183, 0.5813, 1189, 0.5948, 1179, 0.6151, 1164, 0.6422, 
+			1154, 0.6591, 1139, 0.6795, 1118, 0.6896, 1103, 0.6998, 1058, 0.7065, 1083, 0.7133, 1068, 0.7167, 
+			915.4, 0.7180, 1027, 0.7200, 981.5, 0.7203, 747.6, 0.7235, 864.6, 0.7269, 834.2, 0.7302, 869.8, 0.7336, 
+			946.1, 0.737, 976.6, 0.7506, 997.1, 0.754, 910.7, 0.7573, 854.8, 0.7607, 798.9, 0.7641, 687.1, 0.7675, 
+			839.6, 0.7709, 961.7, 0.7712, 921.0, 0.781, 956.7, 0.7946, 906.0, 0.8014, 880.7, 0.8110, 870.6, 0.8115, 
+			784.2, 0.8117, 845.2, 0.8135, 809.6, 0.8149, 758.8, 0.8151, 677.4, 0.8155, 713.0, 0.8160, 733.4, 0.8217, 
+			713.1, 0.8318, 718.3, 0.8352, 769.2, 0.8386, 799.7, 0.8488, 825.2, 0.8691, 805.1, 0.886, 779.8, 0.8928, 
+			713.8, 0.8962, 642.7, 0.8995, 581.7, 0.9063, 536.0, 0.9131, 490.3, 0.9165, 531.0, 0.9165, 592.0, 0.9233, 
+			536.1, 0.9266, 495.5, 0.9266, 429.4, 0.93, 368.4, 0.9334, 276.9, 0.9334, 190.5, 0.9402, 256.7, 0.9436, 282.1, 
+			0.9537, 251.7, 0.9571, 307.7, 0.9605, 378.9, 0.9639, 450.1, 0.9707, 501.0, 0.9774, 557.0, 0.9842, 618.1, 1.001, 
+			592.9, 1.025, 552.4, 1.059, 496.8, 1.093, 451.4, 1.099, 466.8, 1.103, 421.0, 1.106, 375.3, 1.109, 334.7, 1.113, 
+			268.6, 1.12, 172.0, 1.13, 111.1, 1.143, 146.9, 1.16, 253.8, 1.17, 320.0, 1.174, 350.6, 1.187, 360.9, 1.191, 391.4, 
+			1.218, 361.2, 1.235, 366.4, 1.262, 310.8, 1.279, 321.1, 1.292, 311.1, 1.309, 275.7, 1.319, 224.9, 1.33, 169.1, 1.35, 
+			113.4, 1.367, 57.61, 1.38, 6.898, 1.428, 32.8, 1.455, 68.67, 1.468, 63.73, 1.489, 109.7, 1.516, 171.0, 1.54, 227.2,
+			1.567, 217.3, 1.587, 212.4, 1.607, 202.4, 1.628, 202.6, 1.651, 187.6, 1.678, 167.6, 1.702, 162.7, 1.736, 152.9, 
+			1.773, 132.9, 1.797, 102.7, 1.831, 52.16, 1.858, 11.75, 1.946, 38.07, 1.99, 58.86, 2.014, 74.35, 2.051, 74.73, 
+			2.088, 75.11, 2.122, 75.46, 2.149, 70.65, 2.22, 71.37, 2.295, 72.13] # Solar Irradiance [W/m ** 2/µm]
+		solar_spectrum_radiation_distribution = np.reshape(solar_spectrum_radiation_distribution,[len(solar_spectrum_radiation_distribution)//2,2])    
+		f = interpolate.interp1d(solar_spectrum_radiation_distribution.T[0], solar_spectrum_radiation_distribution.T[1])
+		wavelength_um = np.arange(np.min(solar_spectrum_radiation_distribution.T[0]), np.max(solar_spectrum_radiation_distribution.T[0]), 0.001)
+		# low_wl = 0.8
+		# high_wl = 2.0
+		spectrum = f(wavelength_um) # interpolation resulting spectrum
+		# wl_vec_um = np.all([[wavelength <=high_wl ], [wavelength >=low_wl ]], axis=0)
+		# print(wl_vec_um)
+		return spectrum, wavelength_um
+
+	def light_heat_dissipation(self, light=None):
+		return light.Voltage_V * light.Current_A - light.PeakPower_W
+
+	def light_conversion_efficiency(self, light=None):
+		return light.PeakPower_W / (light.Voltage_V * light.Current_A)
+
+	def wallFlux(self, light=None, scene=None, dist_vec=None, lens=None, light_type='ir_pulse'):
 		if light is None:
 			light = self.light
+		if lens is None:
+			lens = self.lens
 		if scene is None:
 			scene = self.scene	
 		if dist_vec is None:
@@ -81,7 +126,22 @@ class Photonic:
 		# print(' ## wallFlux ## \n', light.PeakPower_W,'\n =====  \n' )
 		# Flux on a wall (scene) during lighting time (pulse time)
 		# Flux due to point source attached to the sensor
-		return light.PeakPower_W * light.Transmission / (np.radians(light.Hfov_deg) * np.radians(light.Vfov_deg) * dist_m ** 2)  # [W/m^2]
+		if light_type == 'ir_pulse':
+			flux = light.PeakPower_W * light.Transmission * light.Number_units / \
+				(np.radians(light.Hfov_deg) * np.radians(light.Vfov_deg) * dist_m ** 2)  # [W/m^2]
+		if light_type == 'solar':
+			y, x = self.solarSpectrum_W_m2_nm()
+			f = interpolate.interp1d(x, y)
+			amb = scene.AmbientLight_lux / maximum_amb_light_lux
+			if len(dist_m.shape):
+				solar_flux_W_m2_nm = amb * f([light.WaveLength_nm / 1000])# solar flux input in [um]
+				solar_flux_W_m2_nm_vec = np.array(dist_m.shape[0] * list(solar_flux_W_m2_nm))
+			else:
+				solar_flux_W_m2_nm = amb * f(light.WaveLength_nm / 1000) # solar flux input in [um]
+				solar_flux_W_m2_nm_vec = solar_flux_W_m2_nm
+
+			flux = solar_flux_W_m2_nm_vec * lens.BP_filter_width_nm # [W/m^2]
+		return flux
 
 	def siliconFlux(self, wall_flux, lens=None, scene=None):
 		if lens is None:
@@ -93,19 +153,24 @@ class Photonic:
 		# flux_active = light['peak'] * light['transmission'] / (light['hfov'] * light['vfov'] * scene['distance'] ** 2)  # [W/m^2]
 		return wall_flux * scene.Reflectivity * lens.Transmission / (4 * lens.F_num ** 2)   # [W/m^2]
 
-	def photoelectron(self, siliconFlux, sensor=None, light=None, op=None):
+	def photoelectron(self, siliconFlux, sensor=None, light=None, op=None, light_type='ir_pulse'):
 		if sensor is None:
 			sensor = self.sensor
 		if light is None:
 			light = self.light
 		if op is None:
 			op = self.op
-			
+
+		if light_type == 'ir_pulse':		
+			integration_time_sec = op.InBurstDutyCycle * op.BurstTime_s 
+		if light_type == 'solar':
+			### TODO: need to formulate integration time for solar
+			integration_time_sec = op.InBurstDutyCycle * op.BurstTime_s  
+
 		energy_to_pe = hc / (light.WaveLength_nm * nm) # Conversion from energy [J] to number of photons
 		# PE on silicon from the scene during lighting time (pulse time)
 		pe_per_sec = siliconFlux * (sensor.PixelSize_m ** 2) * sensor.QE * sensor.FF / energy_to_pe  # [photoelectrons / sec] 
-		pe_per_burst = pe_per_sec * op.InBurstDutyCycle * op.BurstTime_s  # [photoelectrons in burst]
-		return pe_per_burst
+		return pe_per_sec * integration_time_sec # [photoelectrons within integration time]
 
 	def siliconFlux2(self, light=None, scene=None, lens=None, dist_vec=None):
 		return self.siliconFlux(wall_flux=self.wallFlux(dist_vec=dist_vec))
@@ -113,12 +178,29 @@ class Photonic:
 	def photoelectron2(self, light=None, scene=None, lens=None, sensor=None, op=None, dist_vec=None):
 		return self.photoelectron(siliconFlux=self.siliconFlux(wall_flux=self.wallFlux(dist_vec=dist_vec)))
 
+	def noise_electron(self, sensor=None, op=None):
+		if sensor is None:
+			sensor = self.sensor
+		if op is None:
+			op = self.op
+
+		### TODO: Find appropriate integration time foe dark signal
+		integration_time_sec = op.InBurstDutyCycle * op.BurstTime_s
+		dark_noise_e = sensor.DarkSignal_e_s * integration_time_sec
+		readout_noise_e = sensor.ReadoutNoise_e
+		return dark_noise_e + readout_noise_e
+
+	def kTC_noise_e(self, C_farad):
+		return 1/e_minus * np.sqrt(k_b * T_kelvin * C_farad) # 1/[coul]*sqrt([V][A][Sec][A][Sec]/[V]) == [1] 
+
+
 	def generate_pulse(self, delay=None, rise=None, fall=None, width=None, time_interval=None, smooth=False, mode='charge_discharge'):
-		'''Create a light pulse (similar to a capacitor charge/discharge)
-			rise - Pulse rise time [sec]
-			fall - Pulse fall time [sec]
-		    width - Pulse width time [sec]
-		    time_interval - Sample time_interval [sec]
+		'''
+		Create a light pulse (similar to a capacitor charge/discharge)
+		rise - Pulse rise time [sec]
+		fall - Pulse fall time [sec]
+		width - Pulse width time [sec]
+		time_interval - Sample time_interval [sec]
 		'''
 		self.time_interval = time_interval
 		if time_interval is None:
@@ -177,11 +259,15 @@ class Photonic:
 		'''
 		return pixel_fill_factor * silicon_responsivity_a_w * hc / (wavelength_m * e_minus) # [ratio] 
 
-
-class Spectra:
-	def __init__():
-		a = 1
-
+	def qe_by_absorption(self, silicon_absorption_cm, pixel_fill_factor, epi_thickness_m):
+		''' 
+		Silicon absorption [1/CM] (changes with wavelength 2000@940nm, 500@850nm)
+		fill factor [ratio]
+		Epitaxial layer thickness [m]
+		[1/cm]*[cm/m]*[m] ==> [1]
+		'''
+		cm_m = 100 # [m/cm]
+		return pixel_fill_factor * (1.0 - np.exp(-silicon_absorption_cm * cm_m * epi_thickness_m)) # [ratio] 
 
 if __name__ == '__main__':
 	import os
@@ -226,8 +312,20 @@ if __name__ == '__main__':
 	tmp = photonic.qe_by_responsivity(silicon_responsivity_a_w=0.002, pixel_fill_factor=1., wavelength_m=1550.*nm)
 	print('####%7.7f'%tmp+'#####')
 
+	alpha = 500 # [1/cm] absorption coef @ 850nm
+	silicon_epi = 10e-6 # [m] 
+	tmp = photonic.qe_by_absorption(pixel_fill_factor=1, silicon_absorption_cm=alpha, epi_thickness_m=silicon_epi)
+	print(f'QE of {1e6*silicon_epi}um EPI thickness @850 nm is {100*tmp:2.0f}%')
+	alpha = 200 # [1/cm] absorption coef @ 940nm
+	tmp = photonic.qe_by_absorption(pixel_fill_factor=1, silicon_absorption_cm=alpha, epi_thickness_m=silicon_epi)
+	print(f'QE of {1e6*silicon_epi}um EPI thickness @940 nm is {100*tmp:2.0f}%')	
+	alpha = 10000 # [1/cm] absorption coef Ge @ 1350nm
+	silicon_epi = 3e-6 # [m] 
+	tmp = photonic.qe_by_absorption(pixel_fill_factor=1, silicon_absorption_cm=alpha, epi_thickness_m=silicon_epi)
+	print(f'QE of {1e6*silicon_epi}um EPI thickness @1350 nm is {100*tmp:2.0f}%')
+
 	print('=====\nWall flux = {:2.3} W/m**2\nSilicon flux = {:2.3}  W/m**2\nPhotoelectron = {:5.5} photonelectron/burst\n======'.format(
-		photonic.wallFlux(), 
+		photonic.wallFlux(light_type='solar'), 
 	    photonic.siliconFlux(wall_flux=photonic.wallFlux()),
 	    photonic.photoelectron(siliconFlux=photonic.siliconFlux(wall_flux=photonic.wallFlux())))) #,'\n',photonic.photoelectron())	
 
@@ -245,6 +343,8 @@ if __name__ == '__main__':
 	y2, t2 = photonic.generate_pulse(delay=delay, rise=rise, fall=fall, width=width, smooth=True)
 
 	y3, t3 = photonic.conv_light_shutter(t_light=t1, y_light=y1, t_shutter=t2, y_shutter=y2)
+
+	print(f'Solar: {photonic.wallFlux(light_type="solar", dist_vec=np.array([1,2]))}')
 
 	import matplotlib.pyplot as plt
 	plt.plot(t1,y1)
